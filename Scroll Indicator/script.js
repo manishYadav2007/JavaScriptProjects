267a648fdda0d62d44c9c6722266e4f8ff0e0384
Manish Yadav <manishyaduvanshi8294@gmail.com>
2025-09-07 13:08:18 +0530

    Add new Project, Scroll Indicator

---
+++ Scroll Indicator/script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+const scrollIndicatorElement  = document.getElementById("scrollIndicator");
+
+const maxScroll = window.document.body.scrollHeight - window.innerHeight; 
+
+
+
+window.addEventListener("scroll", () => {
+    const percentage = ((window.scrollY) / maxScroll) * 100;
+    scrollIndicatorElement.style.width = `${percentage}%`;
+})
+
+
+
+
+
+
+
